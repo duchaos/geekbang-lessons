1e3ca8dbf4e5cef1ab646d831d64aa0e450be2f6
duchao <253466640@qq.com>
2020-03-02 08:18:43 +0800

    init

---
+++ geekbang-lessons/thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.thinking.in.spring.ioc.overview.dependency.injection;
+
+import org.geekbang.thinking.in.spring.ioc.overview.repository.UserRepository;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * 依赖注入示例
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since
+ */
+public class DependencyInjectionDemo {
+
+    public static void main(String[] args) {
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+
+        // 依赖来源一：自定义 Bean
+        //org.springframework.beans.factory.support.AbstractBeanFactory.doGetBean
+        UserRepository userRepository = applicationContext.getBean("userRepository", UserRepository.class);
+
+        System.out.println(userRepository.getUsers());
+
+        // 依赖来源二：依赖注入（內建依赖）
+        //NoSuchBeanDefinitionException:
+        //              No qualifying bean of type 'org.springframework.beans.factory.BeanFactory' available
+        System.out.println(userRepository.getBeanFactory());
+
+
+        ObjectFactory userFactory = userRepository.getObjectFactory();
+
+        System.out.println(userFactory.getObject() == applicationContext);
+
+        // 依赖查找（错误）说明内建依赖是不能通过依赖查找对
+        //System.out.println(beanFactory.getBean(BeanFactory.class));
+
+        // 依赖来源三：容器內建 Bean
+        // StandardEnvironment
+        Environment environment = applicationContext.getBean(Environment.class);
+        System.out.println("获取 Environment 类型的 Bean：" + environment);
+    }
+
+    private static void whoIsIoCContainer(UserRepository userRepository, ApplicationContext applicationContext) {
+
+
+        // ApplicationContext 组合了 BeanFactory ，它是 BeanFactory 企业级实现，增加了n多功能
+        // ConfigurableApplicationContext <- ApplicationContext <- BeanFactory
+        // ConfigurableApplicationContext#getBeanFactory()
+
+
+        // 这个表达式为什么不会成立
+        System.out.println(userRepository.getBeanFactory() == applicationContext);
+
+        // ApplicationContext is BeanFactory
+
+    }
+
+}
